3246dc91bd3ddfc30c777d67c9ad8a571749eb6e
juninalmeida <junioralmeidati2023@gmail.com>
2026-02-17 20:09:07 -0300

    feat: extract ticket creation logic into controller module

--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -1,9 +1,10 @@
+import { create } from "../controllers/tickets/create.js"; 
+
 export const tickets = [
   {
     method: "POST",
     path: "/tickets",
-    controller: (req, res) => {
-      res.end("test route");
-    },
+    controller: create,
+
   },
 ];
