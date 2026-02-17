fc55dc1e0bf43d6da818ee620cad103d453fb677
juninalmeida <junioralmeidati2023@gmail.com>
2026-02-17 19:08:22 -0300

    feat: add JSON handler middleware and wire it into server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
 import http from "node:http";
 
-function listener(req, res) {}
+import { jsonHandler } from "./middlewares/jsonHandler.js";
+
+async function listener(req, res) {
+  await jsonHandler(req, res);
+  console.log(req.body);
+}
 
 http.createServer(listener).listen(3333);
