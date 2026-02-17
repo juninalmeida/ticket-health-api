00d7293f68d2a5e8518b17c2da82393178b8bff0
juninalmeida <junioralmeidati2023@gmail.com>
2026-02-17 19:44:12 -0300

    feat: add route handler middleware for request routing

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import http from "node:http";
 
+import { routeHandler } from "./middlewares/routeHandler.js";
 import { jsonHandler } from "./middlewares/jsonHandler.js";
 
 async function listener(req, res) {
   await jsonHandler(req, res);
-  console.log(req.body);
+
+  routeHandler(req, res);
 }
 
 http.createServer(listener).listen(3333);
