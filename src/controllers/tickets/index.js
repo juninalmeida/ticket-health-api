b3e1d9fcbd885a630ca62304f0f6e5876516437d
juninalmeida <junioralmeidati2023@gmail.com>
2026-02-18 12:53:19 -0300

    feat: parse query params and expose req.query to controllers

--- src/controllers/tickets/index.js
+++ src/controllers/tickets/index.js
@@ -1,6 +1,9 @@
 export function index({ req, res, database }) {
+  const { status } = req.query;
+  
+  console.log(status);
+
   const tickets = database.select("tickets");
 
   return res.end(JSON.stringify(tickets));
 }
-
